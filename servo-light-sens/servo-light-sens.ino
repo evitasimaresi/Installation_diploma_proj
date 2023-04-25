8ce5a0f9b2364a955767941efca37c20f1187d3a
evitasimaresi <evita_96@windowslive.com>
2023-04-25 17:07:03 +0300

    New approach on servo controlling Version v1.3

--- servo-light-sens/servo-light-sens.ino
+++ servo-light-sens/servo-light-sens.ino
@@ -3,19 +3,22 @@
 #include <Servo.h>
 int sensorvalue = 0;
 int sensorvalue_new = 0;
-int rotation =0;
+int rotation = 0;
 Servo myservo;
+Servo myservo2;
 
 int maxL = 500 ; //max light
 int minL = 5;  //min light
 int maxR = 180; //max rotation
 int time = 15; //delay time
-int lim = 15; // + - limits for first if
+int lim = 10; // + - limits for if
 
 
 void setup()
 {
+  //pinMode (13, OUTPUT); //LEDBLINK
   myservo.attach(9);
+  myservo2.attach(10);
   pinMode(A5, INPUT);
   Serial.begin(9600);
   Serial.print ("max light/minlight:");
@@ -32,34 +35,52 @@ void setup()
 
 void loop()
 {
+  //digitalWrite(13, LOW); //LEDBLINK
   sensorvalue_new = analogRead(A5);
-
-
   Serial.print ("light:" );
   Serial.print(sensorvalue);
   Serial.print ("   ");
   Serial.print (" new light:");
   Serial.print(sensorvalue_new);
   Serial.print ("   ");
- 
+
 
   if (sensorvalue_new <= 500 || sensorvalue_new >= 5) {
-    if ( abs(sensorvalue_new - sensorvalue) > 15) {
-      rotation = map(sensorvalue_new, minL , maxL , 1, maxR);
+    if ( abs(sensorvalue_new - sensorvalue) > lim) {
+      if (sensorvalue_new > sensorvalue) {
+        sensorvalue += 5;
+        rotation += 5;
+        if (rotation > 180) {
+          rotation = 180;
+          //digitalWrite(13, HIGH); //LEDBLINK
+        }
+      }
+
+      else {
+        rotation -= 5;
+        sensorvalue -= 5;
+        if (rotation < 0) {
+          rotation = 0;
+        }
+      }
     }
+
   }
   else if (sensorvalue_new < 5) {
     rotation = 0;
   }
   else if (sensorvalue_new > 500) {
     rotation = 180;
+    //digitalWrite(13, HIGH); //LEDBLINK
   }
   myservo.write (rotation);
+  myservo2.write (rotation);
   time = rotation / 5000 ; //delay time
 
-  sensorvalue = sensorvalue_new;
- Serial.print ("rotation:");
+
+  //sensorvalue = sensorvalue_new;
+  Serial.print ("rotation:");
   Serial.println(rotation);
 
-  delay (100);
+  // delay (1000);
 }
