c7e61e6291c3ea27667181dc34015fc582691e6c
evitasimaresi <evita_96@windowslive.com>
2023-04-25 15:30:22 +0300

    New update of version v1.2.

--- servo-light-sens/servo-light-sens.ino
+++ servo-light-sens/servo-light-sens.ino
@@ -3,11 +3,11 @@
 #include <Servo.h>
 int sensorvalue = 0;
 int sensorvalue_new = 0;
-int rotation ;
+int rotation =0;
 Servo myservo;
 
 int maxL = 500 ; //max light
-int minL= 5;   //min light
+int minL = 5;  //min light
 int maxR = 180; //max rotation
 int time = 15; //delay time
 int lim = 15; // + - limits for first if
@@ -33,31 +33,33 @@ void setup()
 void loop()
 {
   sensorvalue_new = analogRead(A5);
-  
-  Serial.print ("light:");
+
+
+  Serial.print ("light:" );
   Serial.print(sensorvalue);
   Serial.print ("   ");
   Serial.print (" new light:");
   Serial.print(sensorvalue_new);
   Serial.print ("   ");
-  if (sensorvalue_new > sensorvalue +lim || sensorvalue_new < sensorvalue -lim ) {
-   if (sensorvalue_new > maxL ) {
-     rotation = maxR;
-     myservo.write (rotation);
-   }
-   else if (sensorvalue_new > minL ){
-     rotation = map(sensorvalue_new, minL , maxL , 1, maxR);
-     myservo.write (rotation);
-   }
-   
-  // time = rotation / 5000; //delay time
+ 
+
+  if (sensorvalue_new <= 500 || sensorvalue_new >= 5) {
+    if ( abs(sensorvalue_new - sensorvalue) > 15) {
+      rotation = map(sensorvalue_new, minL , maxL , 1, maxR);
+    }
   }
-  else if (sensorvalue_new = 0){
-      rotation = 0;
-      myservo.write (rotation);
+  else if (sensorvalue_new < 5) {
+    rotation = 0;
   }
+  else if (sensorvalue_new > 500) {
+    rotation = 180;
+  }
+  myservo.write (rotation);
+  time = rotation / 5000 ; //delay time
+
   sensorvalue = sensorvalue_new;
-  Serial.print ("roation:");
+ Serial.print ("rotation:");
   Serial.println(rotation);
-  delay (time);
+
+  delay (100);
 }
