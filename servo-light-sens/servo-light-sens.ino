3f6570dceea7b29183f710ed07b6954f014292ca
Evita Simaresi <evita_96@windowslive.com>
2023-06-01 17:33:58 +0300

    Make order in the code
    Separate the if for Servos in functions and making the code cleaner

--- servo-light-sens/servo-light-sens.ino
+++ servo-light-sens/servo-light-sens.ino
@@ -17,13 +17,16 @@ int historyIndex = 0;
 // Create a JSON object to store the data
 StaticJsonDocument<200> doc;
 
+//1st Servo
 int sensorvalue = 0;
 int sensorvalue_new = 0;
 int rotation = 0;
-int sensorvalue_2 = 0; // 2nd servo
-int sensorvalue_new_2 = 0; //2nd servo
-int rotation_2 = 0; //2nd servo
 Servo myservo;
+
+//2nd Servo
+int sensorvalue_2 = 0;
+int sensorvalue_new_2 = 0;
+int rotation_2 = 0;
 Servo myservo2;
 
 int maxL = 500 ; //max light
@@ -36,10 +39,14 @@ void setup()
 {
 
   Serial.begin(9600);
+  //1st Servo
   myservo.attach(9);
-  myservo2.attach(10);
   pinMode(A5, INPUT);
+
+  //2nd Servo
+  myservo2.attach(10);
   pinMode(A4, INPUT);
+
   //monitor_print_info ();
 
   // Load the history from EEPROM
@@ -50,8 +57,8 @@ void setup()
 
 void loop()
 {
-  sensorvalue_new = analogRead(A5);
-  sensorvalue_new_2 = analogRead(A4);
+  sensorvalue_new = analogRead(A5); //1st Servo
+  sensorvalue_new_2 = analogRead(A4);//2nd Servo
 
   //Json save 1st servo -------------------------------------
   // Save the current sensor value and rotation to history
@@ -79,12 +86,38 @@ void loop()
 
   //Serial.print ("light:" );
   //Serial.print(sensorvalue_new);
-  //  Serial.print ("   ");
-  //  Serial.print (" new light:");
-  //  Serial.print(sensorvalue_new_2);
-  //  Serial.print ("   ");
+  //Serial.print ("   ");
+  //Serial.print (" new light:");
+  //Serial.print(sensorvalue_new_2);
+  //Serial.print ("   ");
 
-  //first servo if--------------------------------------------
+  //Servos ifs--------------------------------------------
+  firstServo();
+  secondServo();
+  //end of servos --------------------------------------------
+
+  myservo.write (rotation);
+  myservo2.write (rotation_2);
+  //time = rotation / 5000 ; //delay time
+
+  //sensorvalue = sensorvalue_new;
+  //Serial.print ("rotation:");
+  //Serial.println(rotation_2);
+}
+/*void monitor_print_info (){
+  Serial.print ("max light/minlight:");
+  Serial.print (maxL);
+  Serial.print ("/");
+  Serial.print (minL);
+  Serial.print (" | max rotation:");
+  Serial.print (maxR);
+  Serial.print (" | delay time:");
+  Serial.print (time);
+  Serial.print (" | limits for first if:");
+  Serial.println (lim);
+  }*/
+
+void firstServo() {
   if (sensorvalue_new <= 500 || sensorvalue_new >= 5) {
     if ( abs(sensorvalue_new - sensorvalue) > lim) {
       if (sensorvalue_new > sensorvalue) {
@@ -110,14 +143,10 @@ void loop()
   }
   else if (sensorvalue_new > 500) {
     rotation = 180;
-
   }
-  //end of servo --------------------------------------------
+}
 
-
-
-
-  //second servo if--------------------------------------------
+void secondServo() {
   if (sensorvalue_new_2 <= 500 || sensorvalue_new_2 >= 5) {
     if ( abs(sensorvalue_new_2 - sensorvalue_2) > lim) {
       if (sensorvalue_new_2 > sensorvalue_2) {
@@ -144,26 +173,6 @@ void loop()
   else if (sensorvalue_new_2 > 500) {
     rotation_2 = 180;
   }
-  //end of second servo --------------------------------------------
-
-  myservo.write (rotation);
-  myservo2.write (rotation_2);
-  //time = rotation / 5000 ; //delay time
-
-  sensorvalue = sensorvalue_new;
-  //Serial.print ("rotation:");
-  //Serial.println(rotation_2);
 }
-/*
-  void monitor_print_info (){
-  Serial.print ("max light/minlight:");
-  Serial.print (maxL);
-  Serial.print ("/");
-  Serial.print (minL);
-  Serial.print (" | max rotation:");
-  Serial.print (maxR);
-  Serial.print (" | delay time:");
-  Serial.print (time);
-  Serial.print (" | limits for first if:");
-  Serial.println (lim);
-  }*/
+
+
